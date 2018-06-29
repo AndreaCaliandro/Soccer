9fa1311550732d25b16848a9abc606609af0a32f
mieva <michelaievamio@gmail.com>
2018-06-29 08:25:33 -0700

    Updates to the ranking class.

--- ranking.py
+++ ranking.py
@@ -1,14 +1,46 @@
 """ Class to create the ranking of teams according the matches results. The ranking is updated after each giornata"""
-import andas as pd
+import pandas as pd
 
 
 class ranking():
 
-    def load_giornata(self):
-        """ Load the giornate table"""
+    def __init__(self, **kwargs):
+        """ Initialize variables here"""
+        # Initialize an empty df
+        col_names = ['team', 'MP', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'Pt', 'pos']
+        df = pd.DataFrame(columns=col_names)
+        first_day = "Giornata 1"
+        list_of_days = ['Giornata 38', 'Giornata 37']
 
-    def matches_played(self):
+    def load_giornata(self, first_day, df):
+        """ Load the giornate table, select giornate and initialize """
+        # Read from the entire table only lines relative to the same day
+        df_init = df_loaded.loc[df['day'] == first_day]
+        # df_init = df_loaded.loc[df_loaded['day'].isin(list)]
+
+        # Initialize lines based on team name
+        home = df_init['home']
+        visitor = df_init['visitor']
+        df['team'] = pd.concat([home, visitor])
+        df.set_index('team', inplace=True)
+        df = df.fillna(0)
+
+        return df
+
+    def matches_played(self, list_of_days, df):
         """ Number of matches played"""
+        df_info = df_loaded.loc[df_loaded['day'].isin(list_of_days)]
+
+        def mph_count():
+        #Count how many match played at home
+        df["MPH"] = df_loaded.groupby("home").count()
+        # Count how many match played as visitor
+        df["MPV"] = df_loaded.groupby("visitor").count()
+        df = df.fillna(0)
+        # Count total matches
+        df["MP"] = df["MPH"] + df["MPV"]
+
+        return df
 
     def won(self):
         """ Number of matches won"""
@@ -36,3 +68,5 @@ class ranking():
 
     def create_ranking_table(self):
         """ Function to create the final table """
+        # my_df.loc[len(my_df)] = [2, 4, 5]
+        # my_df = my_df.append(my_df2)
